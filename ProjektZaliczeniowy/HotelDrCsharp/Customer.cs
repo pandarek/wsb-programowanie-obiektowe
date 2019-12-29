587601f8a28b2b109dc3edf4a592f4492bc45b67
pzxwsb <pzx92440@studenci.wsb.poznan.pl>
2019-12-29 02:53:26 +0100

    Dodano kontaktowy numer telefonu dla klienta

--- ProjektZaliczeniowy/HotelDrCsharp/Customer.cs
+++ ProjektZaliczeniowy/HotelDrCsharp/Customer.cs
@@ -7,11 +7,14 @@ namespace HotelDrCsharp
         private string _nazwisko;
         //private Adres _adres;
         private int _wiek;
+        private int _telefon;
 
         public string Imie { get => _imie; set => _imie = value; }
-        public string Nazisko { get => _nazwisko; set => _nazwisko = value; }
+        public string Nazwisko { get => _nazwisko; set => _nazwisko = value; }
 
         //public Adres Adres { get => _adres; set => _adres = value; }
+
+        public int Telefon { get => _telefon; set => _telefon = value; }
 
         public Customer()
         {
