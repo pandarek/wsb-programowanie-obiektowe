f7760e49c1a6dad329a8b6a2dde97c5c5a6ba2a0
pzxwsb <pzx92440@studenci.wsb.poznan.pl>
2019-12-31 21:27:36 +0100

    dodano klasę employee

---
+++ ProjektZaliczeniowy/HotelDrCsharp/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotelDrCsharp
+{
+    class Employee
+    {
+        private string _imie;
+        private string _nazwisko;
+        private string _stanowisko;
+        private string _telefon;
+        private string _email;
+        private string _login;
+
+        public string Imie { get => _imie; set => _imie = value; }
+        public string Nazwisko { get => _nazwisko; set => _nazwisko = value; }
+        public string Stanowisko { get => _stanowisko; set => _stanowisko = value; }
+        public string Telefon { get => _telefon; set => _telefon = value; }
+        public string Email { get => _email; set => _email = value; }
+
+        public string login { get => _login; set => _login = value; }
+        
+        public Employee()
+        {
+        }
+    }
+}
